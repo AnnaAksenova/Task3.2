0da21198ed8edcd08efce03874fcb8cd58511093
Anna Aksenova <annaa_aksenova@mail.ru>
2021-05-15 22:25:02 +0300

    Java added

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 2000;
+        int topUp = 1100;
+        if (topUp >= 1000){
+            int bonus = (topUp) / 100;
+            System.out.println(account + bonus);
+            System.out.println(bonus);} else {
+            System.out.println(account);
+        }
+
+    }
+}
